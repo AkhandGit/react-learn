c366f6a99638904051b32f8b20abe458464b198d
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-03-12 17:50:22 +0530

    UseStateHooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+const [a, setA] = useState('Ankit')
+
+const handleClick = () => {
+  setA("Akhand")
+  console.log(a)
+  }
+
   return (
     <div>
-      <h1>GoodDay</h1>
+      <h1>GoodDay {a}</h1>
+      <button onClick={handleClick}>Click</button>
       
     </div>
   )
