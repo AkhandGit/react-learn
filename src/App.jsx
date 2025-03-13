a45549605ee3153b95263b40c8aed8df999cbb2c
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-03-14 01:21:27 +0530

    form handling and manual css without framework for now cause tailwind isnt working

--- src/App.jsx
+++ src/App.jsx
@@ -31,12 +31,43 @@ export default App
 */}
 
 
+{/* This is a useEffect example*/}
+{/*
+
+
 import React from 'react'
 
 const App = () => {
   return (
     <div>
       <h2 className='text-5xl bg-pink-700'>Hello Jerry</h2>
+    </div>
+  )
+}
+
+export default App
+
+
+*/}
+
+
+import React from 'react'
+
+const App = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Form Submitted')
+    console.log(e.target[0].value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input className='namebox' type="text" placeholder='Enter your Name'/>
+        <button className='btn'>Submit</button>
+      </form>
+      
     </div>
   )
 }
