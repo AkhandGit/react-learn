d9444bbc5a4911f32bb8ba1787adcb9e2728154a
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-03-21 17:31:32 +0530

    css and navbar usin rouuttter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
         <nav className='navbar'>
         <h2 className='nav-main'>React-Learning</h2>
         <div className='nav-links'>
-            <h4 className='nav-text'>About</h4>
-            <h4 className='nav-text'>Contact</h4>
-            <h4 className='nav-text'>Services</h4>
-            <h4 className='nav-text'>Your Account</h4>
+            <a href='/' className='Home'>Home</a>
+            <a href="/about" className='About'>About</a>
+            <a href="/contact" className='Contact'>Contact</a>
+            <a href="/product" className='Product'>Product</a>
         </div>
         </nav>
     </div>
