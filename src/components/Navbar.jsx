4d71380d8ff0cc1d6e3392495c029bc2342bc4a2
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-03-21 17:45:35 +0530

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <div className='Navbar'>      
         <nav className='navbar'>
-        <h2 className='nav-main'>React-Learning</h2>
-        <div className='nav-links'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/product'>Product</Link>
-        </div>
+          <h2 className='nav-main'>React-Learning</h2>
+          <div className='nav-links'>
+              <Link to='/'>Home</Link>
+              <Link to='/about'>About</Link>
+              <Link to='/contact'>Contact</Link>
+              <Link to='/product'>Product</Link>
+          </div>
         </nav>
     </div>
   )
