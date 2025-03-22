051a4872cacbec6465a20e2c0ab6b37720856d67
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-03-22 22:44:49 +0530

    c

---
+++ src/context/Usercontext.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Usercontext = () => {
+    const username = 'DedSec';
+  return (
+    
+    <div>
+        <h1>Welcome {username}</h1>
+    </div>
+  )
+}
+
+export default Usercontext
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import Usercontext from './context/Usercontext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <Usercontext>
         <App />
-    </BrowserRouter>
+    </Usercontext>
     
   
 )
